2f24294ccbf0a52639c5331b6dbbb5ee08fe4956
Mateusz Iwaniec <miwaniec11@gmail.com>
2017-04-14 11:24:17 +0200

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='inspectr',
-      version='0.8.0',
+      version='0.9.0',
       description='Code quality InspectR',
       url='',
       author='jchmielewski@teonite.com',
